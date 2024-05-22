904523f565379e3219d6eba7409a167f5a307ba6
herve perel <perelherve@hotmail.com>
2024-05-22 11:38:00 +0200

    auth

---
+++ src/lib/env.ts
@@ -0,0 +1,13 @@
+import zod from 'zod'
+
+const envschema = zod.object({
+    DATABASE_URL: zod.string().min(1),
+    GOOGLE_CLIENT_ID: zod.string().min(1),
+    GOOGLE_CLIENT_SECRET: zod.string().min(1),
+    GITHUB_ID: zod.string().min(1),
+    GITHUB_SECRET: zod.string().min(1),
+    NEXTAUTH_URL: zod.string().min(1),
+    NEXTAUTH_SECRET: zod.string().min(1),
+})
+
+export const env = envschema.parse(process.env)
